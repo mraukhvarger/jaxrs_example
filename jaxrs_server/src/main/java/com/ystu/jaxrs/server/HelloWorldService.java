493ff8b6ee109d1102aaf0e2d37b5208a6dd5988
Maxim Raukhvarger <raukhvarger@gmail.com>
2017-11-18 12:28:04 +0300

    json response

--- jaxrs_server/src/main/java/com/ystu/jaxrs/server/HelloWorldService.java
+++ jaxrs_server/src/main/java/com/ystu/jaxrs/server/HelloWorldService.java
@@ -1,17 +1,41 @@
 package com.ystu.jaxrs.server;
 
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
+import org.codehaus.jettison.json.JSONException;
+import org.codehaus.jettison.json.JSONObject;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Arrays;
+import java.util.Map;
 
 @Path("/hello")
 public class HelloWorldService {
 
     @GET
-    @Path("/{hello}")
-    public Response hello(@PathParam("hello") String hello) {
-        return Response.status(200).entity("Hello, " + (hello != null ? hello : "user")).build();
+    @Path("/all")
+    public Response all() {
+        return Response.status(200).entity("Hello, all!").build();
+    }
+
+
+    @GET
+    @Path("/test")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response test() throws JSONException {
+        JSONObject result = new JSONObject();
+        result.put("list", Arrays.asList(1, 2, 3, 4));
+
+        return Response.status(200).entity(Arrays.asList(1, 2, 3)).build();
+    }
+
+    @GET
+    @Path("/")
+    public Response hello(
+            @DefaultValue("user") @QueryParam("user") String user,
+            @DefaultValue("") @QueryParam("x") String x) {
+        return Response.status(200).entity("Hello, " + user + "<br>" + x).build();
     }
 }
